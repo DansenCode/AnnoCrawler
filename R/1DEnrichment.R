d60ec5fa2723adef6eefbe472a8ef3901d972a86
DansenCode <daniel.dh.hornburg@gmail.com>
2019-12-09 20:21:37 +0000

    First commit

---
+++ R/1DEnrichment.R
@@ -0,0 +1,66 @@
+#########################################################################################
+################################
+#### function to calculate 1 D annotation enrichment
+ODE = function(OneDeAnnotationDF = NULL,
+                            quantities = NULL){
+  # OneDeAnnotationDF: dataframe to work on.
+  # quantities: column thatprovides quantifications
+  # performes 1D annotation enrichment according to Cox et al. NAs are not considered a category but are counted in "Rest"
+  # accepts now also multiple entries per element if separated by ";"
+  DataOut = as.data.frame(matrix(data = NA,nrow = 0,ncol = 0))
+  OneDeAnnotationDF = as.data.frame(OneDeAnnotationDF)
+  r = 1
+  
+  for(k in colnames(OneDeAnnotationDF)[colnames(OneDeAnnotationDF)!=quantities]){
+    categories = unique(unlist(sapply(OneDeAnnotationDF[,k],function(x){str_split(x,";")})))
+    categories = categories[!is.na(categories)]
+    categories = categories[!categories == ""] ## all unique annotaions in a given category
+    
+    AnnoList = sapply(OneDeAnnotationDF[,k],function(x){str_split(x,";")[[1]]}) ## all unique and redundant annotaions in a given category
+    # loop per column
+    for(i in categories){
+      Rest = sapply(AnnoList, function(x){sum(x %in% i) == 0 })
+      SelectedCategory = sapply(AnnoList, function(x){sum(x %in% i) > 0 })
+      
+      x = OneDeAnnotationDF[SelectedCategory,quantities]
+      y = OneDeAnnotationDF[Rest,quantities]
+      
+      ## wilcox test and output
+      wilcox.out = wilcox.test(x = x,y = y, alternative = "two.sided")
+      
+      PvOut = wilcox.out$p.value
+      DeltaMeanOut = mean(x,na.rm = TRUE) - mean(y,na.rm = TRUE) ## difference of means (selection non selection)
+      DeltaMedianOut = median(x,na.rm = TRUE) - median(y,na.rm = TRUE) ## difference of medians (selection non selection)
+      SeleMeanOut = mean(x,na.rm = TRUE) ## mean selection
+      SeleMedianOut = median(x,na.rm = TRUE)  ## median selection
+      
+      
+      ## calculate score according to cox et al
+      R1 = mean(rank(OneDeAnnotationDF[,quantities])[SelectedCategory]) # average ranks within the group under consideration
+      R2 = mean(rank(OneDeAnnotationDF[,quantities])[Rest]) # average ranks of all remaining proteins in the experiment
+      n = nrow(OneDeAnnotationDF) # is the total number of data points
+      
+      ScoreOut = 2 * (( R1 - R2 ) / (n))
+      
+      ## update DF
+      DataOut[r,"Column"] = k
+      DataOut[r,"Category"] = i
+      DataOut[r,"pvals"] = PvOut
+      DataOut[r,"DeltaMean"] = DeltaMeanOut
+      DataOut[r,"DeltaMedian"] = DeltaMedianOut
+      DataOut[r,"SeleMean"] = SeleMeanOut
+      DataOut[r,"SeleMedian"] = SeleMedianOut
+      DataOut[r,"scores"] = ScoreOut
+      DataOut[r,"SelectionSize"] = sum(SelectedCategory)
+      
+      r = r + 1
+    }
+  }
+  # adjust p value
+  
+  DataOut[,"BH"] = p.adjust(DataOut$pvals,method = "BH")
+  
+  return(DataOut)
+}
+#########################################################################################
+################################
---
+++ R/FisherTest.R
@@ -0,0 +1,80 @@
+#### function to calculate fisher exact test enrichments
+FishR = function(data = NULL, # data frame (wide) with features, subselection and matched categorical annotations
+                          AnnoCol = NULL, # Names of columns in "data" that contain annotations
+                          SubSelectionCol = NULL, # Name of columns in "data" that contain logical selection or not
+                          MinCatSize = NULL # set a value for minimal category size to be considered
+){ 
+  # check input
+  if (!is.data.frame(data)) stop("data must be dataframe")
+  if (!is.vector(AnnoCol)) stop("AnnoCol must be vector")
+  if (!is.vector(SubSelectionCol) | length(SubSelectionCol)!=1) stop("SubSelectionCol must be vector of length 1")
+  if (sum(!(AnnoCol %in% colnames(data))) > 0) stop(paste("following columns not found: ",
+                                                          colnames(data)[!(AnnoCol %in% colnames(data))], sep = " "))
+  
+  #                selection   non selection
+  # is feature       One           Two
+  # not feature      Three         Four
+  # odds of selection being feature
+  
+  FisherOutDF = data.frame(matrix(data = NA,nrow = 0,ncol = 0))  
+  
+  ## Loop for counting categories across all annotations
+  for(k in AnnoCol){
+    # find unique
+    Categories = str_split(data[,k],";")
+    UniqueCategories = unique(unlist(Categories))
+    TempDF = data.frame(matrix(data = NA, nrow = length(UniqueCategories), ncol = 0))
+    
+    # loop 
+    for (i in 1 : length(UniqueCategories)){
+      FoundCat = unlist(lapply(Categories,function(x){UniqueCategories[i] %in% x}))
+      TempDF[i,"Annotation"] = k
+      TempDF[i,"Category"] = UniqueCategories[i]
+      TempDF[i,"CategorySize"] = sum( FoundCat )
+      TempDF[i,"One"] = sum( data[,SubSelectionCol] & FoundCat )  ### is feature and selection
+      TempDF[i,"Two"] = sum( !data[,SubSelectionCol] & FoundCat )  ### is feature and non-selection
+      TempDF[i,"Three"] = sum( data[,SubSelectionCol] & !FoundCat ) ### not feature and selection
+      TempDF[i,"Four"] = sum( !data[,SubSelectionCol] & !FoundCat )  ### not feature and non-selection
+    }
+    #concatenate dataframe
+    FisherOutDF = bind_rows(FisherOutDF,TempDF)
+  }
+  
+  # filter out small categories
+  FisherOutDF = FisherOutDF[FisherOutDF$CategorySize > MinCatSize,]
+  
+  # contingency table loop
+  for (i in 1: nrow(FisherOutDF)){
+    ContiTable = matrix(data = unlist(FisherOutDF[i,c("One", "Two", "Three", "Four")]),
+                        nrow = 2, ncol = 2, byrow = TRUE)
+    FisherOut = fisher.test(ContiTable)
+    #fill ne tall df with annotation, log ods and p values
+    FisherOutDF[i,"pvalue"] = FisherOut$p.value
+    FisherOutDF[i,"OddsRatio"] = FisherOut$estimate
+    ## PERSEUS style
+    IntersectionSize = FisherOutDF[i,"One"]
+    TotalSize =  sum(FisherOutDF[i,c("One", "Two", "Three", "Four")])
+    SelectionSize = sum(FisherOutDF[i,c("One", "Three")])
+    CategorySize = sum(FisherOutDF[i,c("One", "Two")])
+    FisherOutDF[i,"Enrichment"] = ((IntersectionSize)*(TotalSize))/((SelectionSize)*(CategorySize))
+  }
+  
+  # adjust p value
+  FisherOutDF[,"BH"] = p.adjust(FisherOutDF$pvalue,method = "BH")
+  
+  # log odds
+  FisherOutDF[,"Log2_Odds"] = log2(FisherOutDF$OddsRatio)
+  
+  #flag infinite odds and set to max value
+  FisherOutDF[,"Log2_OddsFinite"] = is.finite(FisherOutDF[,"Log2_Odds"])
+  
+  # set infinite to maximum
+  DynamicRange = FisherOutDF$Log2_Odds[is.finite(FisherOutDF[,"Log2_Odds"])]
+  FisherOutDF$Log2_Odds[which(FisherOutDF$Log2_Odds == -Inf)] = min(DynamicRange)
+  FisherOutDF$Log2_Odds[which(FisherOutDF$Log2_Odds == Inf)] = max(DynamicRange)
+  
+  return(FisherOutDF)
+  
+}
+#########################################################################################
+################################
