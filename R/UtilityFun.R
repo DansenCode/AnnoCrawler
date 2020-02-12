4f8b762ed5648cce5c41d6f5b06ddc98d4500715
DansenCode <54958465+DansenCode@users.noreply.github.com>
2020-02-12 13:29:35 -0800

    Update UtilityFun.R
    Added option to use custom separators

--- R/UtilityFun.R
+++ R/UtilityFun.R
@@ -14,25 +14,28 @@ AnnoCount = function(data = NULL,separator = ";"){
 #########################################################################################
 ################################
 AnnoPuller2 = function(InputVector = NULL,
+                       InputVectorSep = ";",
                        AnnoID = NULL,
-                       AnnoAnno = NULL){
+                       AnnoIDSep = ";",
+                       AnnoAnno = NULL,
+                       OutputSep = ";"){
   ## InputVector is a vector of that is supposed to be interogated for matching annotations. multiple ids (e.g. protei groups) should be separeted by ";"
   ## AnnoID is the vector into which the InputVector is matches and should align to the AnnoAnno, multiple annotations should be splitted by;
   ## AnnoAnno is the vector that contains the annotations and should align with AnnoID                                                      
   ## extract all Annotation IDs       
-  AnnoUniprot = sapply(AnnoID,function(x){str_split(x,";")[[1]]})
+  AnnoUniprot = sapply(AnnoID,function(x){str_split(x,AnnoIDSep)[[1]]})
   
   ## loop through annotations of interest
   OutVector = c()
   for(i in 1: length(InputVector)) {
     
     ## find index
-    ProtGroup = str_split(InputVector[i],";")[[1]]
+    ProtGroup = str_split(InputVector[i],InputVectorSep)[[1]]
     AnnoIndex = unlist(lapply(AnnoUniprot,function(x){sum(x %in% ProtGroup) > 0}))
     
     ## pull all annotations & make unique
     ## making it unique is important since some proteins in a protein group have exactly the same annotations
-    UniqueAnno = paste(unique(AnnoAnno[AnnoIndex]),collapse=";")
+    UniqueAnno = paste(unique(AnnoAnno[AnnoIndex]),collapse=OutputSep)
     
     ## concatenate                    
     OutVector = c(OutVector,UniqueAnno)
